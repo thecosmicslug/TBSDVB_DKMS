ed7de1a5a101017f605fcf3b34d3c5e72733b54f
Danny Pearmain <thecosmicslug@gmail.com>
2025-05-04 11:55:21 +0100

    dkms build working

--- make.sh
+++ make.sh
@@ -1,19 +1,12 @@
 #!/bin/sh
-wget -N http://www.tbsdtv.com/download/document/linux/tbs-tuner-firmwares_v1.0.tar.bz2
-tar jxvf tbs-tuner-firmwares_v1.0.tar.bz2 -C /lib/firmware/
-
 git fetch
 git submodule update --recursive --remote
-
 cd media_build 
 if [ -z "$kernelver" ]; then kernelver=`uname -r` ; fi
-
 make rmmod
 make dir DIR=../media KERNELRELEASE=$kernelver
 make distclean
 make allyesconfig KERNELRELEASE=$kernelver
 sed -i -r 's/(^CONFIG.*_RC.*=)./\1n/g' v4l/.config
 sed -i -r 's/(^CONFIG.*_IR.*=)./\1n/g' v4l/.config
-
-echo "Compiling TBS Drivers..."
 make -j$(grep -c ^processor /proc/cpuinfo 2>/dev/null) KERNELRELEASE=$kernelver
