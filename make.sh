37fb9279f5927f49a7e8108e7f6320ee8b9af693
Danny Pearmain <thecosmicslug@gmail.com>
2025-05-05 13:51:30 +0100

    more tests

--- make.sh
+++ make.sh
@@ -10,4 +10,6 @@ make allyesconfig KERNELRELEASE=$kernelver
 #cp dkms_built.config v4l/.config
 sed -i -r 's/(^CONFIG.*_RC.*=)./\1n/g' v4l/.config
 sed -i -r 's/(^CONFIG.*_IR.*=)./\1n/g' v4l/.config
+#CONFIG_SND_USB_AUDIO ?
+sed -i -r 's/CONFIG_SND_USB_AUDIO=m/CONFIG_SND_USB_AUDIO=y/' v4l/.config
 make -j$(grep -c ^processor /proc/cpuinfo 2>/dev/null) KERNELRELEASE=$kernelver
