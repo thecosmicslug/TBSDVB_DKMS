fb712b3ce9154a9d0b168d5d6b78b3ca0e3d2d9e
Danny Pearmain <thecosmicslug@gmail.com>
2025-05-05 14:19:38 +0100

    revert config change

--- make.sh
+++ make.sh
@@ -7,9 +7,6 @@ make rmmod
 make dir DIR=../media KERNELRELEASE=$kernelver
 make distclean
 make allyesconfig KERNELRELEASE=$kernelver
-#cp dkms_built.config v4l/.config
 sed -i -r 's/(^CONFIG.*_RC.*=)./\1n/g' v4l/.config
 sed -i -r 's/(^CONFIG.*_IR.*=)./\1n/g' v4l/.config
-#CONFIG_SND_USB_AUDIO ?
-sed -i -r 's/CONFIG_SND_USB_AUDIO=m/CONFIG_SND_USB_AUDIO=y/' v4l/.config
 make -j$(grep -c ^processor /proc/cpuinfo 2>/dev/null) KERNELRELEASE=$kernelver
