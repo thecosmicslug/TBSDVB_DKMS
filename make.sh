e3d719f511d31fb91670b847e18ffdee2261b4ea
Danny Pearmain <thecosmicslug@gmail.com>
2025-05-06 15:04:18 +0100

    testing

--- make.sh
+++ make.sh
@@ -10,6 +10,5 @@ make allyesconfig KERNELRELEASE=$kernelver
 sed -i -r 's/(^CONFIG.*_RC.*=)./\1n/g' v4l/.config
 sed -i -r 's/(^CONFIG.*_IR.*=)./\1n/g' v4l/.config
 # Try force snd-usb-audio module
-sed -i -r 's/^\(CONFIG_SND_USB=\).*/\1m/' v4l/.config
-sed -i -r 's/^\(CONFIG_SND_USB_AUDIO=\).*/\1m/' v4l/.config
+sed -i -r 's/(^CONFIG_SND_USB.*=).*/\1m/g' v4l/.config
 make -j$(grep -c ^processor /proc/cpuinfo 2>/dev/null) KERNELRELEASE=$kernelver
