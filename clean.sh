84c3a22121dd1584d61f317e3d39f2651a415dc5
TheCosmicSlug <thecosmicslug@gmail.com>
2026-04-24 11:59:59 +0100

    Refactor build scripts and update module configurations for TBS drivers

--- clean.sh
+++ clean.sh
@@ -2,7 +2,3 @@
 git remote update
 git pull
 git reset --hard
-git submodule foreach git remote update
-git submodule foreach git pull
-git submodule foreach git reset --hard
-git submodule foreach git clean -fx -fd
--- install.sh
+++ install.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
 make install KERNELRELEASE=$kernelver
+if [ -f tbs-tuner-firmwares*.tar.bz2 ]; then
+		tar jxvf tbs-tuner-firmwares*.tar.bz2 -C /lib/firmware/
+else
+		echo "firmware file not exist!!!"		
+fi
